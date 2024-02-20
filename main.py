66583716aa7779879668636b14df8d7a96c3f0f9
mathegudzithend <32202313-mathegudzithend@users.noreply.replit.com>
2024-02-20 06:42:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+r = int(input("Enter %:"))
+R = r//10
+strg = "="*R
+print(f"[{strg:<10}]")
+
+#version 02
+space = " "*(10 - R)
+print(f"[{strg}{space}]")
+quote = "wdwvwqfwfqfwdqqfq"
+print(quote[-2:-1])
---
+++ new.py
@@ -0,0 +1,3 @@
+name = input("Enter in degrees Fahrenheit: ")
+name = int(name)
+print((name-32)*( 5/9))
