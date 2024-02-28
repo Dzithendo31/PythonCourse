697e8500d11d68e048923841e8ab955563b7f6f2
Dzithendo31 <mathegu.dzithendo@gmail.com>
2024-02-28 10:00:37 +0200

    stated with your assignemtnet walk thru and other important stuff

--- day07OOP.py
+++ day07OOP.py
@@ -1,67 +1,75 @@
 from datetime import datetime
+
+
 class Car:
-    #add the values
-    def __init__(self, name,engine,doors):
+    # add the values
+    def __init__(self, name, engine, doors):
         self.name = name
         self.engine = engine
         self.doors = doors
-    
+
     def horn(self):
         return "Helloe Babbi"
-    
 
 
-
-ferrari = Car("Benx","V*",23)
+ferrari = Car("Benx", "V*", 23)
 
 print(ferrari.horn())
 
 
 ##Bank Account
-class bankAccount():
-    def __init__(self, AccNo,AccHolder,balance):
+class bankAccount:
+    intrestRate = 0.02
+
+    def __init__(self, AccNo, AccHolder, balance):
         self.AccountNum = AccNo
         self.Name = AccHolder
-        self.balance = balance
+        self.__balance = balance
         self.transID = 0
         self.trans = []
-    
+
     def displayBalance(self):
-        return f"Your Balance is R{self.balance}"
-    def withdraw(self,amount):
-        
-        if amount > self.balance:
-            return f"Unsuccesful balance is {self.balance}"
+        return f"Your Balance is R{self.__balance}"
+
+    def withdraw(self, amount):
+
+        if amount > self.__balance:
+            return f"Unsuccesful balance is {self.__balance}"
         else:
-            #it there, the money is there..
-            self.balance = self.balance - amount
-            self.trans.append(self.saveTransaction("Withdraw",amount))
-            return f"Successful, Balnace is {self.balance}"
-        
-    def deposit(self,amount):
-        self.balance = self.balance + amount
-        self.trans.append(self.saveTransaction("Withdraw",amount))
-        return f"Successful, Balance is now {self.balance}"
-    
+            # it there, the money is there..
+            self.__balance = self.__balance - amount
+            self.trans.append(self.saveTransaction("Withdraw", amount))
+            return f"Successful, {self.displayBalance()}"
+
+    def deposit(self, amount):
+        self.__balance = self.__balance + amount
+        self.trans.append(self.saveTransaction("Withdraw", amount))
+        return f"Successful, {self.displayBalance()}"
+
     def getBankStatement(self):
         print(f"#  {'id':^6}{'Date':^10}{'Amount':^10}")
         for tran in self.trans:
             print(f"#  {tran['id']:^6}{tran['time']:^10}{tran['amount']:^10}")
+        print(f"\n{'Balance:':#>16}{self.__balance:>10}")
 
-    def saveTransaction(self,type,amount):
-        #Get the time we got
+    def saveTransaction(self, type, amount):
+        # Get the time we got
         item = {}
-        item['id'] = self.transID + 1
-        item['time'] = f"{datetime.now():%d %b}"
-        item['type'] = type
-        item['amount'] = amount
+        item["id"] = self.transID + 1
+        item["time"] = f"{datetime.now():%d %b}"
+        item["type"] = type
+        item["amount"] = amount
         return item
 
+    def applyIntrest(self):
+        self.__balance = self.__balance + self.__balance * self.intrestRate
 
 
-tina = bankAccount(125674234,"Thina",153_000)
+tina = bankAccount(125674234, "Thina", 153_000)
 
-#withdraw
+# withdraw
 tina.withdraw(3000)
 tina.withdraw(3000)
+tina.getBankStatement()
+tina.applyIntrest()
 tina.getBankStatement()
